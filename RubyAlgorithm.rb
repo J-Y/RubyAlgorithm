11957c22084e9df8f96cc3c3df524fba402d4bdd
Jason Zhang <jason.zhang@marketboomer.com>
2015-02-12 18:12:11 +0800

    Update some description for sort method

--- RubyAlgorithm.rb
+++ RubyAlgorithm.rb
@@ -115,7 +115,13 @@ class RubyAlgorithm
 #TODO 冒泡排序
 #百科:http://baike.baidu.com/view/254413.html?wtp=tt
 #Wiki:http://zh.wikipedia.org/zh-tw/%E5%86%92%E6%B3%A1%E6%8E%92%E5%BA%8F
+  # 冒泡排序算法的运作如下：（从后往前）
+  # 比较相邻的元素。如果第一个比第二个大，就交换他们两个。
+  # 对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。在这一点，最后的元素应该会是最大的数。
+  # 针对所有的元素重复以上的步骤，除了最后一个。
+  # 持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。[1]
   def bubble_sort(arr)   #FIXME awesome one
+    #最大的数最先排出来，放到最后，直到最小的元素得出
     1.upto(arr.length-1) { |i| (arr.length-i).times { |j| arr[j], arr[j+1] = arr[j+1], arr[j] if arr[j] > arr[j+1] } }
     arr
   end
@@ -125,7 +131,7 @@ class RubyAlgorithm
   def bubble_sort!
     f = 1
     while f < self.length
-      (0...(self.length-f)).to_a.each do |i|
+      (0...(self.length-f)).to_a.each do |i|  #equal: (0...(self.length-f)).to_a.each == (self.length-f).times
         self[i], self[i+1] = self[i+1], self[i] if self[i] > self[i+1]
       end
       f += 1
@@ -151,15 +157,30 @@ class RubyAlgorithm
 #TODO 插入排序
 #百科:http://baike.baidu.com/view/396887.html?wtp=tt
 #Wiki:http://zh.wikipedia.org/zh-tw/%E6%8F%92%E5%85%A5%E6%8E%92%E5%BA%8F
-  def insertion_sort(array)   #FIXME awesome one
+  # 步骤
+  # ⒈从有序数列和无序数列{a2,a3，…，an}开始进行排序；
+  # ⒉处理第i个元素时（i=2,3，…，n），数列{a1,a2，…，ai-1}是已有序的，而数列{ai,ai+1，…，an}是无序的。用ai与ai-1，a i-2，…，a1进行比较，找出合适的位置将ai插入；
+  # ⒊重复第二步，共进行n-i次插入处理，数列全部有序。
+  # 思路
+  # 假定这个数组的序是排好的，然后从头往后，如果有数比当前外层元素的值大，则将这个数的位置往后挪，直到当前外层元素的值大于或等于它前面的位置为止.这具算法在排完前k个数之后，可以保证a[1…k]是局部有序的，保证了插入过程的正确性.
+
+  # 一般来说，插入排序都采用in-place在数组上实现。具体算法描述如下：
+  # ⒈ 从第一个元素开始，该元素可以认为已经被排序
+  # ⒉ 取出下一个元素，在已经排序的元素序列中从后向前扫描
+  # ⒊ 如果该元素（已排序）大于新元素，将该元素移到下一位置
+  # ⒋ 重复步骤3，直到找到已排序的元素小于或者等于新元素的位置
+  # ⒌ 将新元素插入到下一位置中
+  # ⒍ 重复步骤2~5
+  # 如果比较操作的代价比交换操作大的话，可以采用二分查找法来减少比较操作的数目。该算法可以认为是插入排序的一个变种，称为二分查找排序。
+  def insertion_sort(array)   #FIXME awesome one已提交百度词条
     array.each_with_index do |element, index|
+      next if index == 0 #第一个元素默认已排序
       j = index - 1
-      while j >= 0
-        break if array[j] <= element
-        array[j + 1] = array[j]
+      while j >= 0 && array[j] > element #查找扫描已排序的ary前j个元素，逆序直到第一个元素
+        array[j + 1] = array[j] #否则把大于自己的前一个元素值赋值给后一个元素，依次后移元素直到找到元素需要插入的位置
         j -= 1
       end
-      array[j + 1] = element
+      array[j + 1] = element   #根据上面循环break跳出的时候的j值，找到最后即将插入值的位置就是这个元素后的位置，赋值
     end
     array
   end
@@ -182,11 +203,10 @@ class RubyAlgorithm
 #TODO 选择排序
 #百科:http://baike.baidu.com/view/547263.html?wtp=tt
 #Wiki:http://zh.wikipedia.org/zh-tw/%E9%80%89%E6%8B%A9%E6%8E%92%E5%BA%8F
+  # 选择排序（Selection sort）是一种简单直观的排序算法。它的工作原理是每一次从待排序的数据元素中选出最小（或最大）的一个元素，存放在序列的起始位置，直到全部待排序的数据元素排完。 选择排序是不稳定的排序方法（比如序列[5， 5， 3]第一次就将第一个[5]与[3]交换，导致第一个5挪动到第二个5后面）。
   def selection_sort(array)
     result = []
-    array.size.times do
-      result << array.delete_at(array.index(array.min)) #delete_at of Array return that element deleted
-    end
+    array.size.times { result << array.delete_at(array.index(array.min)) } #delete_at of Array return that element deleted
     result
   end
 
@@ -194,10 +214,16 @@ class RubyAlgorithm
 #TODO Shell排序
 #百科:http://baike.baidu.com/view/549624.html?wtp=tt
 #Wiki:http://zh.wikipedia.org/zh-tw/%E5%B8%8C%E5%B0%94%E6%8E%92%E5%BA%8F
+  # 希尔排序(Shell Sort)是插入排序的一种。也称缩小增量排序，是直接插入排序算法的一种更高效的改进版本。希尔排序是非稳定排序算法。该方法因DL．Shell于1959年提出而得名。
+  # 希尔排序是把记录按下标的一定增量分组，对每组使用直接插入排序算法排序；随着增量逐渐减少，每组包含的关键词越来越多，当增量减至1时，整个文件恰被分成一组，算法便终止。[1]
+  # 先取一个小于n的整数d1作为第一个增量，把文件的全部记录分组。所有距离为d1的倍数的记录放在同一个组中。先在各组内进行直接插入排序；然后，取第二个增量d2<d1重复上述的分组和排序，直至所取的增量 =1( < …<d2<d1)，即所有记录放在同一组中进行直接插入排序为止。
+  # 该方法实质上是一种分组插入方法
+  # 比较相隔较远距离（称为增量）的数，使得数移动时能跨过多个元素，则进行一次比[2] 较就可能消除多个元素交换。D.L.shell于1959年在以他名字命名的排序算法中实现了这一思想。算法先将要排序的一组数按某个增量d分成若干组，每组中记录的下标相差d.对每组中全部元素进行排序，然后再用一个较小的增量对它进行，在每组中再进行排序。当增量减到1时，整个要排序的数被分成一组，排序完成。
+  # 一般的初次取序列的一半为增量，以后每次减半，直到增量为1。
   def shell_sort(array)
     gap = array.size
     while gap > 1
-      gap = gap / 2
+      gap = gap / 2  #分组对应比较，如果前面一组的对应元素大于或等于后一组的元素，则交换彼此的值
       (gap..array.size-1).each do |i|
         j = i
         while j > 0
@@ -213,6 +239,9 @@ class RubyAlgorithm
 #TODO 合并排序
 #百科:http://baike.baidu.com/view/3668564.html?wtp=tt
 #Wiki:http://zh.wikipedia.org/zh-tw/%E5%90%88%E4%BD%B5%E6%8E%92%E5%BA%8F
+  # 合并排序是建立在归并操作上的一种有效的排序算法。该算法是采用分治法（Divide and Conquer）的一个非常典型的应用。
+  # 合并排序法是将两个（或两个以上）有序表合并成一个新的有序表，即把待排序序列分为若干个子序列，每个子序列是有序的。然后再把有序子序列合并为整体有序序列。
+  # 将已有序的子序列合并，得到完全有序的序列；即先使每个子序列有序，再使子序列段间有序。若将两个有序表合并成一个有序表，称为2-路归并。合并排序也叫归并排序。
 # def merge(l, r)
 #   result = []
 #   while l.size > 0 and r.size > 0 do
@@ -258,6 +287,7 @@ class RubyAlgorithm
 #TODO 堆排序
 #百科:http://baike.baidu.com/view/157305.html?wtp=tt
 #Wiki:http://zh.wikipedia.org/zh-tw/%E5%A0%86%E6%8E%92%E5%BA%8F
+  # 堆排序(Heapsort)是指利用堆积树（堆）这种数据结构所设计的一种排序算法，它是选择排序的一种。可以利用数组的特点快速定位指定索引的元素。堆分为大根堆和小根堆，是完全二叉树。大根堆的要求是每个节点的值都不大于其父节点的值，即A[PARENT[i]] >= A[i]。在数组的非降序排序中，需要使用的就是大根堆，因为根据大根堆的要求可知，最大的值一定在堆顶。
   def heapify(a, idx, size)
     left_idx = 2 * idx + 1
     right_idx = 2 * idx + 2
@@ -325,7 +355,8 @@ class RubyAlgorithm
 #TODO 快速排序
 #百科:http://baike.baidu.com/view/115472.html?wtp=tt
 #Wiki:http://zh.wikipedia.org/zh-tw/%E5%BF%AB%E9%80%9F%E6%8E%92%E5%BA%8F
-  def quick_sort(a)
+  # 通过一趟排序将要排序的数据分割成独立的两部分，其中一部分的所有数据都比另外一部分的所有数据都要小，然后再按此方法对这两部分数据分别进行快速排序，整个排序过程可以递归进行，以此达到整个数据变成有序序列。
+  def quick_sort(a) #已加入百度磁条
     (x=a.pop) ? quick_sort(a.select { |i| i <= x }) + [x] + quick_sort(a.select { |i| i > x }) : []
   end
 
@@ -361,6 +392,14 @@ end
 #TODO 计数排序
 #百科:http://baike.baidu.com/view/1209480.html?wtp=tt
 #Wiki:http://zh.wikipedia.org/zh-tw/%E8%AE%A1%E6%95%B0%E6%8E%92%E5%BA%8F
+  # 计数排序对输入的数据有附加的限制条件：
+  # 1、输入的线性表的元素属于有限偏序集S；
+  # 2、设输入的线性表的长度为n，|S|=k（表示集合S中元素的总数目为k），则k=O(n)。
+  # 在这两个条件下，计数排序的复杂性为O(n)。
+  # 计数排序的基本思想是对于给定的输入序列中的每一个元素x，确定该序列中值小于x的元素的个数。一旦有了这个信息，就可以将x直接存放到最终的输出序列的正确位置上。例如，如果输入序列中只有17个元素的值小于x的值，则x可以直接存放在输出序列的第18个位置上。当然，如果有多个元素具有相同的值时，我们不能将这些元素放在输出序列的同一个位置上，因此，上述方案还要作适当的修改。
+  # 假设输入的线性表L的长度为n，L=L1,L2,..,Ln；线性表的元素属于有限偏序集S，|S|=k且k=O(n)，S={S1,S2,..Sk}；则计数排序可以描述如下：
+  # 1、扫描整个集合S，对每一个Si∈S，找到在线性表L中小于等于Si的元素的个数T(Si)；
+  # 2、扫描整个线性表L，对L中的每一个元素Li，将Li放在输出线性表的第T(Li)个位置上，并将T(Li)减1。
   def counting_sort(array)
     min, max = array.min, array.max
     counts = Array.new(max-min+1, 0)
@@ -374,6 +413,7 @@ end
 #TODO 基數排序
 #百科:http://baike.baidu.com/view/1170573.html?wtp=tt
 #Wiki:http://zh.wikipedia.org/zh-tw/%E5%9F%BA%E6%95%B0%E6%8E%92%E5%BA%8F
+  # 基数排序法又称“桶子法”（bucket sort）或bin sort，顾名思义，它是透过键值的部份资讯，将要排序的元素分配至某些“桶”中，藉以达到排序的作用，基数排序法是属于稳定性的排序，其时间复杂度为O (nlog(r)m)，其中r为所采取的基数，而m为堆数，在某些时候，基数排序法的效率高于其它的稳定性排序法。
   def kth_digit(n, i)
     while i > 1
       n = n / 10
@@ -403,6 +443,7 @@ end
 #TODO 桶排序
 #百科:http://baike.baidu.com/view/1784217.html?wtp=tt
 #Wiki:http://zh.wikipedia.org/zh-tw/%E6%A1%B6%E6%8E%92%E5%BA%8F
+  # 桶排序 (Bucket sort)或所谓的箱排序，是一个排序算法，工作的原理是将数组分到有限数量的桶子里。每个桶子再个别排序（有可能再使用别的排序算法或是以递归方式继续使用桶排序进行排序）。桶排序是鸽巢排序的一种归纳结果。当要被排序的数组内的数值是均匀分配的时候，桶排序使用线性时间（Θ（n））。但桶排序并不是 比较排序，他不受到 O(n log n) 下限的影响。
   def quick_sort1(a)
     (x=a.pop) ? quick_sort1(a.select { |i| i <= x }) + [x] + quick_sort1(a.select { |i| i > x }) : []
   end
@@ -425,6 +466,8 @@ end
 
 #TODO 鸡尾酒排序(>_<)
 #wikipedia 详解: http://en.wikipedia.org/wiki/Cocktail_sort
+# 使用鸡尾酒排序为一列数字进行排序的过程可以通过右图形象的展示出来：
+# 数组中的数字本是无规律的排放，先找到最小的数字，把他放到第一位，然后找到最大的数字放到最后一位。然后再找到第二小的数字放到第二位，再找到第二大的数字放到倒数第二位。以此类推，直到完成排序。
 def cocktail_sort!
   f  = 0
   while f < self.length/2
@@ -837,5 +880,8 @@ end
 # 在java种 -1 % 12的余数是－1
 # 而在ruby和 python种 -1 % 12 的余数为11
 # 这个特性用在数组元算的选择上就显得非常巧妙
+
+
+#TODO Aha algorithm
 
 #TODO ruby quiz issues
