fc5f06d91915ee3cf7a2a5c411cb8def9541af59
Jason Zhang <jason.zhang@marketboomer.com>
2015-02-12 14:21:47 +0800

    add interview issues(to be continuted..)

---
+++ RubyInterviews.rb
@@ -0,0 +1,38 @@
+﻿1.项目中用到哪些技术和哪些gem??
+
+2.项目中遇到的大挑战以及如何解决？？
+
+3.AngularJS,其他常用的gems
+
+4.项目中用到几个layout以及asset中如何导入？分批导入？
+
+5.form自动验证插件，ajaxsubmit
+
+6.升级rails4注意事项
+
+7.怎样防止多次点击button（processing bar -> disable; button属性在点击的时候disable并且显示登陆。。。）
+
+8.常用的jquery 插件：qtip，autocomplete, jqgrid, datepicker, popupwindow, validate 用法；
+
+9.开源项目：
+12 weeks 12 projects
+ruby china
+onestep(好奇猫)
+blog eye
+writings
+quora
+19wu
+islandzero
+email_spider
+
+10.常用技巧
+
+11.常用算法
+
+12.环境安装，配置
+
+13.发布部署
+
+14.Respec
+
+15.Postgresql view
